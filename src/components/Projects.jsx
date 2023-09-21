27e1a920d2ad47e75a1eac7e238f54583c8e88f6
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-09-21 19:25:23 +0300

    Vercel deployment fix

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -74,6 +74,7 @@ const Projects = () => {
                   <Image
                     className='w-full h-full object-cover'
                     src={budgitProjectPicture}
+                    alt='BudgIT Project Picture'
                     width={1920}
                     height={1080}
                   />
@@ -90,11 +91,11 @@ const Projects = () => {
                   </h3>
                   <p className='font-normal text-md'>
                     Innotopia is a company based in Groningen, Netherlands that
-                    is a rebrand of the company 'Indietopia'. The new company
-                    expands on the previous business model and offers all kind
-                    of different creative digital solutions with a focus on
-                    digital transformation and innovation acceleration. The
-                    development process includes creating design, selecting
+                    is a rebrand of the company &apos;Indietopia&apos;. The new
+                    company expands on the previous business model and offers
+                    all kind of different creative digital solutions with a
+                    focus on digital transformation and innovation acceleration.
+                    The development process includes creating design, selecting
                     relevant technologies based on the client requirements,
                     development and testing.
                   </p>
@@ -129,6 +130,7 @@ const Projects = () => {
                   <Image
                     className='w-full h-full object-contain'
                     src={innotopiaProjectPicture}
+                    alt='Innotopia Project Picture'
                     width={1920}
                     height={1080}
                   />
